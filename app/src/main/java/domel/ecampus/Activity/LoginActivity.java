1933a601e388867395c8284cdd8514a31c6b7e22
jorgemt94 <jorgemt10@hotmail.com>
2016-05-05 21:56:48 +0200

    layout welcome & login

---
+++ app/src/main/java/domel/ecampus/Activity/LoginActivity.java
@@ -0,0 +1,20 @@
+package domel.ecampus.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import domel.ecampus.R;
+
+/**
+ * Created by jorgemt94 on 5/5/16.
+ */
+public class LoginActivity  extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
---
+++ app/src/main/java/domel/ecampus/Activity/WelcomeActivity.java
@@ -0,0 +1,18 @@
+package domel.ecampus.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import domel.ecampus.R;
+
+
+public class WelcomeActivity  extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+    }
+}
