1c2e63e685f9fefc179d2166576e946893679b27
Guillermo <ga2468@hotmail.com>
2016-05-06 19:28:10 +0200

    new values added.
    Created view 8 layout.

---
+++ app/src/main/java/domel/ecampus/Activity/AddStudentActivity.java
@@ -0,0 +1,15 @@
+package domel.ecampus.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import domel.ecampus.R;
+
+public class AddStudentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_student);
+    }
+}
