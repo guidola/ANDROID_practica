548317451c8036146a3a8c792910cc53b9592a80
jorgemt94 <jorgemt10@hotmail.com>
2016-05-06 12:35:48 +0200

    login plantilla

--- app/src/main/java/domel/ecampus/Activity/LoginActivity.java
+++ app/src/main/java/domel/ecampus/Activity/LoginActivity.java
@@ -5,9 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 
 import domel.ecampus.R;
 
-/**
- * Created by jorgemt94 on 5/5/16.
- */
 public class LoginActivity  extends AppCompatActivity {
 
 
