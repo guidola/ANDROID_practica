81fad97244c13e66cecfe518271652e66baa9264
Guillermo <ga2468@hotmail.com>
2016-05-06 13:30:27 +0200

    extra dimensions added.
    Created view 9 layout.

---
+++ app/src/main/java/domel/ecampus/Activity/StudentActivity.java
@@ -0,0 +1,15 @@
+package domel.ecampus.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import domel.ecampus.R;
+
+public class StudentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student);
+    }
+}
