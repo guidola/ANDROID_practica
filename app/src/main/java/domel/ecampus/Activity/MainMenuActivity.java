e8a92516f2effebc7fc84f323bc33f12a3b8f925
jorgemt94 <jorgemt10@hotmail.com>
2016-05-10 10:24:30 +0200

    MainMenuActivity

--- app/src/main/java/domel/ecampus/Activity/MainMenuActivity.java
+++ app/src/main/java/domel/ecampus/Activity/MainMenuActivity.java
@@ -1,7 +1,11 @@
 package domel.ecampus.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import domel.ecampus.R;
 
@@ -11,5 +15,51 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        LinearLayout subject = (LinearLayout)this.findViewById(R.id.linear_subjects);
+        LinearLayout student = (LinearLayout)this.findViewById(R.id.linear_students);
+        LinearLayout exam = (LinearLayout)this.findViewById(R.id.linear_exam);
+        LinearLayout logout = (LinearLayout)this.findViewById(R.id.linear_logout);
+
+        subject.setOnClickListener(new View.OnClickListener() {
+            //go to subject manager activity
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainMenuActivity.this, SubjectManagerActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        student.setOnClickListener(new View.OnClickListener() {
+            //go to subject manager activity
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainMenuActivity.this, StudentManagerActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        exam.setOnClickListener(new View.OnClickListener() {
+            //go to subject manager activity
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainMenuActivity.this, ExamsListActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //logout
+                Intent intent = new Intent(MainMenuActivity.this, LoginActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+
+            }
+        });
     }
 }
