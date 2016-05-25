594b00ce8951266c1b705d1b2a7d475e17e4c898
jorgemt94 <jorgemt10@hotmail.com>
2016-05-26 00:03:32 +0200

    toolbar menu done

--- app/src/main/java/domel/ecampus/Activity/MainMenuActivity.java
+++ app/src/main/java/domel/ecampus/Activity/MainMenuActivity.java
@@ -3,10 +3,10 @@ package domel.ecampus.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import domel.ecampus.R;
 
@@ -16,6 +16,43 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        //over flow menu (logout)
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        // Inflate a menu to be displayed in the toolbar
+        toolbar.inflateMenu(R.menu.overflow_menu_toolbar);
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.student_manager:
+                        Intent intent_st = new Intent(MainMenuActivity.this, StudentManagerActivity.class);
+                        startActivity(intent_st);
+                        return true;
+                    case R.id.subject_manager:
+                        Intent intent_sbj = new Intent(MainMenuActivity.this, SubjectManagerActivity.class);
+                        startActivity(intent_sbj);
+                        return true;
+                    case R.id.exam:
+                        Intent intent_ex = new Intent(MainMenuActivity.this, ExamsListActivity.class);
+                        startActivity(intent_ex);
+                        return true;
+                    case R.id.logout:
+                        Intent intent_log = new Intent(MainMenuActivity.this, LoginActivity.class);
+                        intent_log.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent_log);
+                        finish();
+                        return true;
+                    default:
+                        break;
+                }
+                return true;
+            }
+        });
+
 
         LinearLayout subject = (LinearLayout)this.findViewById(R.id.linear_subjects);
         LinearLayout student = (LinearLayout)this.findViewById(R.id.linear_students);
@@ -75,4 +112,5 @@ public class MainMenuActivity extends AppCompatActivity {
         setIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(setIntent);
     }
+
 }
